b885ccf30c034a0b1c7a0e81dcd7226d6b8a2672
Oded Arbel <oded@geek.co.il>
2020-12-01 18:52:40 +0200

    expose github auth to manual checkout

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -ex
 
 [ -f /etc/redmine-compose/configuration ] && . /etc/redmine-compose/configuration
+if [ -n "$USERNAME" ]; then
+	GITHUBAPI="https://${USERNAME}:${PASSWORD}@api.github.com"
+else
+	GITHUBAPI="https://api.github.com"
+fi
 
 function install_from_github() {
 	local repo="$1" bundler="$2" targetdir="$3" githubauth
 	read username reponame <<<"${repo/\// }"
 	targetdir="${targetdir:-$reponame}"
-	if [ -n "$USERNAME" ]; then
-		githubauth="${USERNAME}:${PASSWORD}@"
-	fi
 	[ -d "$targetdir" ] && rm -rf $targetdir
 	(
 		set -eo pipefail
-		curl -sfL -D/dev/stderr https://${githubauth}api.github.com/repos/"$repo"/tarball | tar -zx --xform="s,$username-$reponame-[[:alnum:]]*,$targetdir,"
+		curl -sfL -D/dev/stderr $GITHUBAPI/repos/"$repo"/tarball | tar -zx --xform="s,$username-$reponame-[[:alnum:]]*,$targetdir,"
 		cd "$targetdir"
 		[ -n "$bundler" ] && rm -f Gemfile.lock && bundle
 		exit 0
@@ -37,7 +39,7 @@ install_from_github haru/redmine_code_review
 install_from_github speedy32129/time_logger
 
 (
-  curl -sfL https://api.github.com/repos/onozaty/redmine-view-customize/tarball | tar -zx --xform="s,onozaty-redmine-view-customize-[[:alnum:]]*,view_customize,"
+  curl -sfL $GITHUBAPI/repos/onozaty/redmine-view-customize/tarball | tar -zx --xform="s,onozaty-redmine-view-customize-[[:alnum:]]*,view_customize,"
   cd view_customize
   bundle config set without 'development test'
   (echo "source 'https://rubygems.org'"; cat Gemfile) > Gemfile.new; mv Gemfile.new Gemfile
